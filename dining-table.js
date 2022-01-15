460fd99ed570882d06c2879bb04699fa3100c506
shivansh payak <75551006+151616@users.noreply.github.com>
2022-01-15 13:48:34 -0500

    Add files via upload

---
+++ dining-table.js
@@ -0,0 +1,43 @@
+img = "";
+Status = "";
+object = [];
+function setup(){
+canvas = createCanvas(640, 420);
+canvas.center();
+object_detector = ml5.objectDetector("cocossd", modelloaded);
+}
+function preload(){
+    img  = loadImage('dining-table.JPG');
+}
+function draw(){
+    image(img,0,0,640,420);
+    if(Status != ""){
+        for(i = 0; i < object.length; i++){
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+            fill("#FF0000");
+            stroke("#FF0000");
+            percent = floor(object[i].confidence *100);
+            text(object[i].label + "  " + percent + "%", object[i].x, object[i].y + 15); 
+            noFill();
+            rect(object[i].x, object[i].y, object[i].width, object[i].height);
+        }
+    }
+    
+    }
+function modelloaded(){
+    console.log("Model Loaded!");
+    Status = "true";
+    object_detector.detect(img, gotresults);
+}
+function gotresults(error, results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        object = results;
+    }
+}
+function back(){
+    location = "index.html";
+}
+
---
+++ main.js
@@ -0,0 +1,18 @@
+function trophies(){
+window.location = "trophies.html";
+}
+function bed(){
+window.location = "bed.html";
+}
+function fish_aquarium(){
+window.location = "fish-aquarium.html";
+}
+function dining_table(){
+window.location = "dining-table.html";
+}
+function painting(){
+window.location = "pictures.html";
+}
+function tea(){
+window.location = "tea.html";
+}
